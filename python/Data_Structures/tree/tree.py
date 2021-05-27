fe4e5a22bf967403c7da0357132aa1a7c6fdeaea
Nizar Alsaeed <nizar.ayman@gmail.com>
2021-05-28 00:18:50 +0300

    challenge15

---
+++ python/Data_Structures/tree/tree.py
@@ -0,0 +1,149 @@
+import os
+import sys
+sys.path.append(os.path.join(os.path.dirname(__file__),'../'))
+from stacks_and_queues.stacks_and_queues import Stack,Queue
+#if you don't have it: https://github.com/NizarAlsaeed/data-structures-and-algorithms/blob/main/python/Data_Structures/stacks_and_queues/stacks_and_queues.py
+
+class Node_BT:
+    def __init__(self, value=None):
+        """ properties for the value stored in the node, the left child node, and the right child node."""
+        self.value = value
+        self.left = None
+        self.right = None
+
+    def __str__(self):
+        return self.value
+
+class  BinaryTree:
+    def __init__(self, root=None):
+        self.root = root
+
+    """the same tree but the order of the returned node is different for each method"""
+    def preOrder(self)->list:
+        """depth first traversals"""
+        ans = []
+        if self.root is not None:
+            def _print(node):
+                ans.append(node.value)
+                if node.left:
+                    _print(node.left)
+                if node.right:
+                    _print(node.right)
+
+            _print(self.root)
+            return ans
+        else:
+            raise Exception('The tree is empty')
+
+
+    def inOrder(self)->list:
+        """depth first traversals"""
+        ans = []
+        if self.root is not None:
+            def _print(node):
+                if node.left:
+                    _print(node.left)
+
+                ans.append(node.value)
+
+                if node.right:
+                    _print(node.right)
+
+            _print(self.root)
+            return ans
+        else:
+            raise Exception('The tree is empty')
+
+    def postOrder(self)->list:
+        """depth first traversals"""
+        ans=[]
+        if self.root is not None:
+            def _print(node):
+                if node.left:
+                    _print(node.left)
+                if node.right:
+                    _print(node.right)
+                ans.append(node.value)
+
+            _print(self.root)
+            return ans
+        else:
+            raise Exception('The tree is empty')
+
+    def __str__(self):
+        """the default is to print the preOrder list"""
+        return self.preOrder()
+
+
+
+class BinarySearchTree(BinaryTree):
+
+    def __init__(self, root=None):
+        super().__init__(root=root)
+
+    def add (self, value):
+        """adds a new node with that value in the correct location in the binary search tree."""
+
+        new_node_BT = Node_BT(value)
+
+        def traverse_and_add(root):
+            if root.value >= value:
+                if not root.left:
+                    #print(f'L of {root.value}')
+                    setattr(root, 'left', new_node_BT)
+                else:
+                    traverse_and_add(root.left)
+            else:
+                if not root.right:
+                    #print(f'R of {root.value}')
+                    setattr(root, 'right', new_node_BT)
+                else:
+                    traverse_and_add(root.right)
+
+        traverse_and_add(self.root)
+
+
+    def contains (self, value):
+        """returns a boolean indicating whether or not the value is in the tree at least once."""
+        root = self.root
+        while root:
+            if root.value == value:
+                return True
+            elif root.value > value:
+                if root.left:
+                    root = root.left
+                else: return False
+
+            else:
+                if root.right:
+                    root = root.right
+                else: return False
+        return False
+
+
+if __name__=='__main__':
+    abs_root = Node_BT('a')
+
+    abs_root.left = Node_BT('b')
+    abs_root.right = Node_BT('c')
+
+    abs_root.left.left = Node_BT('d')
+    abs_root.left.right = Node_BT('e')
+
+    abs_root.right.left = Node_BT('f')
+
+    binary_tree = BinaryTree(abs_root)
+    binary_tree.preOrder()
+    print('--pre')
+    binary_tree.inOrder()
+    print('--in')
+    binary_tree.postOrder()
+    print('--post')
+
+    binary_search_tree = BinarySearchTree(Node_BT(23))
+    binary_search_tree.add(15)
+    binary_search_tree.add(4)
+    binary_search_tree.add(90)
+
+    binary_search_tree.preOrder()
+    print(binary_search_tree.contains(15))
---
+++ python/tests/test_tree.py
@@ -0,0 +1,38 @@
+from Data_Structures.tree.tree import Node_BT ,BinaryTree, BinarySearchTree
+
+
+def test_empty_tree():
+    """    Can successfully instantiate an empty tree"""
+    binary_tree = BinaryTree()
+    actual = binary_tree.root
+    expected = None
+    assert actual == expected
+
+def test_single_node_tree():
+    """    Can successfully instantiate a tree with a single root node"""
+    binary_tree = BinaryTree(Node_BT('True'))
+    actual = binary_tree.root.value
+    expected = 'True'
+    assert actual == expected
+
+def test_add():
+    """    Can successfully add a left child and right child to a single root node"""
+    binary_tree = BinarySearchTree(Node_BT(2))
+    binary_tree.add(1)
+    binary_tree.add(3)
+    actual = binary_tree.preOrder()
+    expected = [2,1,3]
+    assert actual == expected
+
+
+def test_order():
+    """    Can successfully return a collection from a preorder traversal
+    Can successfully return a collection from an inorder traversal
+    Can successfully return a collection from a postorder traversal
+"""
+    binary_tree = BinarySearchTree(Node_BT(2))
+    binary_tree.add(1)
+    binary_tree.add(3)
+    actual = [binary_tree.preOrder(), binary_tree.inOrder(), binary_tree.postOrder()]
+    expected = [[2,1,3], [1,2,3], [1,3,2]]
+    assert actual == expected
