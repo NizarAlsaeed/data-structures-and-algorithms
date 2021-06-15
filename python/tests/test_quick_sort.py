5745368b7b292a77d3900f75756e963cfc30122d
NizarAlsaeed <nizar.ayman@gmail.com>
2021-06-15 23:42:58 +0300

    fixed test

--- python/tests/test_quick_sort.py
+++ python/tests/test_quick_sort.py
@@ -3,6 +3,7 @@ import pytest
 from Data_Structures.quick_sort.quick_sort import QuickSort
 
 def test_quick_sort():
-    actual = QuickSort([8,4,23,42,16,15])
+    arr= [8,4,23,42,16,15]
+    actual = QuickSort(arr, 0, len(arr)-1)
     expected = [4,8,15,16,23,42]
     assert actual == expected
